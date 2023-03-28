ea4275836e3c25d9c9e34649ffe65df831fb9979
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-03-28 19:59:56 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
+"use strict"
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems.reduce(
+    (acc, { original, preview, description }) =>
+      (acc += `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`),
+    ''
+  );
+  
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 
-console.log(galleryItems);
